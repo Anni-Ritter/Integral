d5b02e97530bc800bdd6d778de0aba15eb1046ba
AnNi_RiTtEr <57864367+Anni-Ritter@users.noreply.github.com>
2021-06-04 15:19:04 +0300

    update project

--- Integral/Program.cs
+++ Integral/Program.cs
@@ -14,7 +14,7 @@ namespace Integral
                 e: 0.001
             );
             integral.Sol();
-            Thread.Sleep(1000);
+            Thread.Sleep(2000);
             Console.WriteLine("Интеграл 1/ln(x), промежуток от 2 до 5, число промежутков " + Environment.ProcessorCount.ToString());
             Console.WriteLine($"Результат: {integral.Result}");
             Console.ReadKey();
