ad556564642965d8f1e5c8271ef461c13bc99b5b
Anni_Ritter <afon.anastasia@mail.ru>
2021-06-04 15:06:25 +0300

    update project

--- Integral/Program.cs
+++ Integral/Program.cs
@@ -7,73 +7,17 @@ namespace Integral
     {
         static void Main(string[] args)
         {
-
-            double[] result = new double[20];
-            const double b = 5;
-            const double a = 2;
-            double[] arr = new double[20];
+            var integral = new Solution(
+                f: x => 1 / Math.Log(x),
+                a: 2,
+                b: 5,
+                e: 0.001
+            );
+            integral.Sol();
+            Thread.Sleep(1000);
             Console.WriteLine("Интеграл 1/ln(x), промежуток от 2 до 5, число промежутков " + Environment.ProcessorCount.ToString());
-            int num = Environment.ProcessorCount;
-            double h = (b - a) / num;
-            for (int i = 0; i <= num; i++)
-            {
-                arr[i] = a + i * h;
-            }
-            Thread[] t = new Thread[num + 1];
-            for (int i = 1; i <= num; i++)
-            {
-                int x = i;
-                Thread thr = new Thread(delegate () { Rect(arr[x - 1], arr[x], 0.0001, result, x); });
-                t[i] = thr;
-                t[i].Start();
-            }
-            
-            var watch = System.Diagnostics.Stopwatch.StartNew();
-            for (int i = 1; i <= num; i++)
-            {
-                t[i].Join();
-                result[num + 1] += result[i];
-            }
-            Console.WriteLine("Результат: " + result[num + 1]);
-            watch.Stop();
-            var elapsedMs = watch.Elapsed;
-            Console.WriteLine("Время вычисления: " + elapsedMs);
+            Console.WriteLine($"Результат: {integral.Result}");
             Console.ReadKey();
-        }
-        static void Rect(double a, double b, double e, double[] sarr, int num)
-        {
-            int n = 2;
-            double Jn, x, s;
-            double Jn1 = 0;
-            do
-            {
-                double h = (b - a) / n;
-                x = a + h;
-                s = 0;
-                for (int i = 0; i <= (n - 1); i++)
-                {
-                    double f = Function(x);
-                    s += f;
-                    x += h;
-                }
-                Jn = s * h;
-                if (Math.Abs(Jn1 - Jn) <= e)
-                {
-                    sarr[num] = Jn;
-                    return;
-                }
-                else
-                {
-                    Jn1 = Jn;
-                    n = 2 * n;
-                }
-            }
-            while (true);
-        }
-        static double Function(double x)
-        {
-            double rez = 1 / Math.Log(x);
-            return rez;
         }
     }
 }
---
+++ Integral/Solution.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Integral
+{
+    public class Solution
+    {
+        private Func<double, double> func;
+
+        private double leftSide;
+        private double rightSide;
+        private double eps;
+
+        private Semaphore semaphore;
+
+        private double result;
+
+        public double Result
+        {
+            get { return result; }
+        }
+
+        public Solution(Func<double, double> f, double a, double b, double e)
+        {
+            func = f;
+            semaphore = new Semaphore(Environment.ProcessorCount, Environment.ProcessorCount);
+            leftSide = a;
+            rightSide = b;
+            eps = e;  
+        }
+
+        private void Rect(object args)
+        {
+            semaphore.WaitOne();
+
+            var left = (double)((object[])args)[0];
+            var right = (double)((object[])args)[1];
+
+            var distance = right - left;
+
+            var h = distance / 2;
+            var center = left + h;
+
+            if (distance < eps)
+            {
+                result += func(center) * distance;
+            }
+            else
+            {
+                var threadLeft = new Thread(Rect);
+                threadLeft.Start(new object[] { left, center });
+
+                var threadRight = new Thread(Rect);
+                threadRight.Start(new object[] { center, right });
+            }
+
+            semaphore.Release();
+        }
+        public void Sol()
+        {
+            var thread = new Thread(Rect);
+            thread.Start(new object[] { leftSide, rightSide });
+        }
+    }
+}
